db357a86ece9808c616bb47664219411e74b56f8
JKereliuk <hydradjango@gmail.com>
2015-01-31 14:01:01 -0500

    added quiet mode

--- dMetronome/wearApp/src/main/java/com/digutsoft/metronome/DMCMetronome.java
+++ dMetronome/wearApp/src/main/java/com/digutsoft/metronome/DMCMetronome.java
@@ -1,24 +1,3 @@
-/*
- *
- * Copyright (c) 2014 Digutsoft.
- * http://www.digutsoft.com/
- *
- * This file is part of dMetronome and it was originally from android-metronome.
- * Visit http://www.digutsoft.com/apps/product.php?id=metronome to know more about dMetronome,
- * and Google android-metronome to know more about original code.
- *
- * dMetronome is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * It is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- */
-
 package com.digutsoft.metronome;
 
 import android.app.Activity;
@@ -42,6 +21,7 @@ public class DMCMetronome {
 
     Vibrator mVibrator;
     SharedPreferences mSharedPreferences;
+    boolean quietMode = false;
 
     View mBackground;
     TextView mTvTempo;
@@ -51,6 +31,8 @@ public class DMCMetronome {
     int mPeriod;
     long mTickDuration;
     boolean isFlashEnabled, alwaysOnStatus;
+    // set this somwhere
+    boolean isClient;
 
     Context mContext;
 
@@ -64,6 +46,7 @@ public class DMCMetronome {
     }
 
     public void startTick(int ticksPerSec) {
+        quietMode = false;
         mRunning = true;
         mCount = 0;
         //mPeriod is count default set to 4
@@ -84,37 +67,48 @@ public class DMCMetronome {
     private void tick() {
         if (!mRunning) return;
 
-        //mPeriod is count not sure why we need != 1
-        if (mCount - mPeriod == 0) {
-            mCount = 0;
-            if (isFlashEnabled) {
-                mBackground.setBackgroundColor(Color.parseColor("#000000"));
-                mTvTempo.setTextColor(Color.parseColor("#ffffff"));
+            //mPeriod is count not sure why we need != 1
+            if (mCount - mPeriod == 0) {
+                mCount = 0;
+                if(!quietMode) {
+                    if (isFlashEnabled) {
+                        mBackground.setBackgroundColor(Color.parseColor("#000000"));
+                        mTvTempo.setTextColor(Color.parseColor("#ffffff"));
+                    }
+                    mVibrator.vibrate(120);
+                }
+                else {
+                    if (isFlashEnabled) {
+                        mBackground.setBackground(mDefaultBackground);
+                        mTvTempo.setTextColor(Color.parseColor("#000000"));
+                    }
+                    mVibrator.vibrate(100);
+                }
             }
-            mVibrator.vibrate(120);
-        } else {
-            if (isFlashEnabled) {
-                mBackground.setBackground(mDefaultBackground);
-                mTvTempo.setTextColor(Color.parseColor("#000000"));
-            }
-            mVibrator.vibrate(100);
-        }
 
-        //calls the Handler with the delay of mTickDuration for example 60000 / 60bpm = a tick every 1 second
-        mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG), mTickDuration);
+            //calls the Handler with the delay of mTickDuration for example 60000 / 60bpm = a tick every 1 second
+            mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG), mTickDuration);
     }
 
     public void stopTick() {
-        mRunning = false;
-        mCount = 0;
-        mBackground.setBackground(mDefaultBackground);
-        mTvTempo.setTextColor(Color.parseColor("#000000"));
-        //I think removeMessages means make MSG 0 or something???
-        mHandler.removeMessages(MSG);
-
-        if (alwaysOnStatus) {
-            ((Activity) mContext).getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        if(clientMode) {
+            quietMode = true;
         }
+        else {
+            mRunning = false;
+            mCount = 0;
+            mBackground.setBackground(mDefaultBackground);
+            mTvTempo.setTextColor(Color.parseColor("#000000"));
+            //I think removeMessages means make MSG 0 or something???
+            mHandler.removeMessages(MSG);
+
+            if (alwaysOnStatus) {
+                ((Activity) mContext).getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        }
+    }
+    public boolean getQuietMode() {
+        return quietMode;
     }
 
 
